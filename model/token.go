cae41a32bd1aca85e37607cdb574eccf9db005d2
苏青安 <992182040@qq.com>
2025-09-18 14:54:15 +0800

    refactor(core): 调整数据库模型注释

--- model/token.go
+++ model/token.go
@@ -13,7 +13,7 @@ type Token struct {
 	AccessToken  string         `gorm:"uniqueIndex;not null;comment:访问Token" json:"access_token"`
 	RefreshToken string         `gorm:"uniqueIndex;not null;comment:刷新Token" json:"refresh_token"`
 	ExpiresAt    time.Time      `gorm:"not null;comment:过期时间" json:"expires_at"`
-	CreatedAt    time.Time      `json:"created_at"`              // 创建时间，自动填充
-	UpdatedAt    time.Time      `json:"updated_at"`              // 更新时间，自动更新
-	DeletedAt    gorm.DeletedAt `gorm:"index" json:"deleted_at"` // 逻辑删除
+	CreatedAt    time.Time      `json:"created_at"`
+	UpdatedAt    time.Time      `json:"updated_at"`
+	DeletedAt    gorm.DeletedAt `gorm:"index" json:"deleted_at"`
 }
--- model/user.go
+++ model/user.go
@@ -10,14 +10,14 @@ import (
 
 // User 用户表
 type User struct {
-	ID        uint           `gorm:"primaryKey;autoIncrement" json:"id"`         // 用户ID
-	Name      string         `gorm:"size:100;not null" json:"name"`              // 名称
-	Email     string         `gorm:"size:100;uniqueIndex;not null" json:"email"` // 邮箱
-	Password  string         `gorm:"size:255;not null" json:"password"`          // 密码
+	ID        uint           `gorm:"primaryKey;autoIncrement" json:"id"`
+	Name      string         `gorm:"size:100;not null" json:"name"`
+	Email     string         `gorm:"size:100;uniqueIndex;not null" json:"email"`
+	Password  string         `gorm:"size:255;not null" json:"password"`
 	Salt      string         `gorm:"size:64;not null;comment:随机盐" json:"salt"`
-	CreatedAt time.Time      `json:"created_at"`              // 创建时间，自动填充
-	UpdatedAt time.Time      `json:"updated_at"`              // 更新时间，自动更新
-	DeletedAt gorm.DeletedAt `gorm:"index" json:"deleted_at"` // 逻辑删除
+	CreatedAt time.Time      `json:"created_at"`
+	UpdatedAt time.Time      `json:"updated_at"`
+	DeletedAt gorm.DeletedAt `gorm:"index" json:"deleted_at"`
 }
 
 // 验证密码是否正确
